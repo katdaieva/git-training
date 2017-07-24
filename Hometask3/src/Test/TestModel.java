d1c69f0ee1ddc0e41fd0b7d57d59fa5961ff8db6
katdaieva <katoatd@gmail.com>
2017-07-25 00:39:50 +0300

    New tests added

--- Hometask3/src/Test/TestModel.java
+++ Hometask3/src/Test/TestModel.java
@@ -5,13 +5,38 @@ package Test;
 
 import MoreOrLess.Model;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestModel {
+    private Model m;
+
+    @Before
+    public void runT(){
+        m = new Model();
+    }
+
     @Test
     public void testRand() {
-        Model m = new Model();
         int secretNumber  = m.rand(0,100);
         if ((secretNumber >= 100) | (secretNumber <= 0)) Assert.fail();
+    }
+
+    @Test
+    public void testGiveLessAnswer() {
+        String answer = m.giveAnswer(50,77);
+        Assert.assertEquals(answer, "Less");
+    }
+
+    @Test
+    public void testGiveMoreAnswer() {
+        String answer = m.giveAnswer(80,77);
+        Assert.assertEquals(answer, "More");
+    }
+
+    @Test
+    public void testGiveVictoryAnswer() {
+        String answer = m.giveAnswer(80,80);
+        Assert.assertEquals(answer, "Yes");
     }
 }
