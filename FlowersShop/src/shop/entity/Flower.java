482d7190035b9216f2f540b2cfd732c1fd457e40
katdaieva <katoatd@gmail.com>
2017-07-27 01:49:41 +0300

    The class for flowers was created

---
+++ FlowersShop/src/shop/entity/Flower.java
@@ -0,0 +1,17 @@
+package shop.entity;
+
+/**
+ * Created by User on 27.07.2017.
+ */
+public class Flower extends Plant {
+    boolean isMultiFlower;
+
+    public Flower(String name, int count, boolean isMultiFlower) {
+        super(name, count);
+        this.isMultiFlower = isMultiFlower;
+    }
+
+    public String toString() {
+        return this.name + ": there " + (isMultiFlower == false ? "is one one flower." : "are more flowers!");
+    }
+}
