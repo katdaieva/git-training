cab65965741b93eacc2a4434a9f149d1ce1ecefb
katdaieva <katoatd@gmail.com>
2017-07-26 23:20:39 +0300

    Task 2. Game "More or Less?"

---
+++ MoreOrLess/src/Model.java
@@ -0,0 +1,20 @@
+/**
+ * Created by User on 26.07.2017.
+ */
+import java.lang.Math;
+
+public class Model {
+    public int rand (int min, int max) {return min + (int) (Math.random() * max);}
+    public int rand () {return (int) (Math.random() * 100);}
+    public int secretNumber;
+    public int newNumber;
+
+    public boolean giveAnswer (int newNumber, int secretNumber) {
+        if (newNumber == secretNumber) return true;
+        else return false;
+    }
+    public String comparing (int newNumber, int secretNumber) {
+        if (newNumber < secretNumber) return "left";
+        else return "right";
+    }
+}
