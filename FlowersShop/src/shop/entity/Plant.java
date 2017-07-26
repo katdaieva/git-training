26835b67059fdef9c7577abf357ea0fa7df7e43b
katdaieva <katoatd@gmail.com>
2017-07-27 01:37:08 +0300

    Task 5. Flower shop. Start

---
+++ FlowersShop/src/shop/entity/Plant.java
@@ -0,0 +1,15 @@
+package shop.entity;
+
+/**
+ * Created by User on 27.07.2017.
+ */
+public class Plant {
+    String name;
+    int count;
+
+    public Plant(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+    
+}
